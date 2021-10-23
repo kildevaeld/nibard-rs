05530bc8b504027be29a5b9343a38674fe0fd0d4
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2021-10-24 00:33:21 +0200

    fix export

--- nibard-query/src/lib.rs
+++ nibard-query/src/lib.rs
@@ -13,8 +13,8 @@ mod target;
 mod types;
 
 pub use self::{
-    b, column::*, column_ext::*, context::*, error::*, expression::*, filter::*, join::*,
-    select::*, selection::*, statement::*, table::*, target::*, types::*,
+    column::*, column_ext::*, context::*, error::*, expression::*, filter::*, join::*, select::*,
+    selection::*, statement::*, table::*, target::*, types::*,
 };
 
 #[cfg(test)]

8b75279b23ee46fea6ca1c33f6c5fa6e4f3bbcc6
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2021-10-23 23:18:21 +0200

    send sync

--- nibard-query/src/statement.rs
+++ nibard-query/src/statement.rs
@@ -4,7 +4,13 @@ pub trait Statement {
     fn build(&self, ctx: &mut Context<'_>) -> Result<(), Error>;
 }
 
-impl Statement for Box<dyn Statement> {
+impl Statement for Box<dyn Statement + Send> {
+    fn build(&self, ctx: &mut Context<'_>) -> Result<(), Error> {
+        (**self).build(ctx)
+    }
+}
+
+impl Statement for Box<dyn Statement + Send + Sync> {
     fn build(&self, ctx: &mut Context<'_>) -> Result<(), Error> {
         (**self).build(ctx)
     }

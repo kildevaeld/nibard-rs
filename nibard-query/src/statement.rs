0003f8f72dbfb2e87f2af51cca2b7e98a4e76d6e
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2021-10-23 23:12:52 +0200

    impl statement for bix

--- nibard-query/src/statement.rs
+++ nibard-query/src/statement.rs
@@ -3,3 +3,9 @@ use super::{Context, Error};
 pub trait Statement {
     fn build(&self, ctx: &mut Context<'_>) -> Result<(), Error>;
 }
+
+impl Statement for Box<dyn Statement> {
+    fn build(&self, ctx: &mut Context<'_>) -> Result<(), Error> {
+        (**self).build(ctx)
+    }
+}
